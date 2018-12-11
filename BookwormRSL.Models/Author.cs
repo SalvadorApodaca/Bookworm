6abb866cd216a7e4a02bd398688f1e9854881af4
SalvadorApodaca <salvapodaca@gmail.com>
2018-12-10 21:30:40 -0800

    Models first version

---
+++ BookwormRSL.Models/Author.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookwormRSL.Models
+{
+    public class Author
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AuthFirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AuthLastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public DateTime DeathDay { get; set; }
+
+        [MaxLength(255)]
+        public string PicturePath { get; set; }
+    }
+}
---
+++ BookwormRSL.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookwormRSL.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Key]
+        public string Name { get; set; }
+
+        public ICollection<Genre> Genres { get; set; }
+    }
+}
